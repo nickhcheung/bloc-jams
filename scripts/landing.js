69b67495196b14b87b49a73ece845b95c2ac1aea
Nick Cheung <nickhcheung@hotmail.com>
2017-02-27 23:56:55 -0800

    Checkpoint 16 Landing Page: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,28 @@
-var pointsArray = document.getElementsByClassName("point");
 
-
-var animatePoints = function(points){
+var animatePoints = function(){
 
   var revealPoint = function(){
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)";
-    points[i].style.msTransform = "scaleX(1) translateY(0)";
-    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    $(this).css({
+      opacity: 1,
+      transform: "scaleX(1) translateY(0)"
+    });
   };
 
-  for(var i = 0; i < points.length; i++) {
-    revealPoint(i);
-  };
+  $.each($(".point"), revealPoint);
 
 };
 
-window.onload = function(){
-  if(window.innerHeight > 950){
-    animatePoints(pointsArray);
+$(window).load(function(){
+  if($(window).height() > 950){
+    animatePoints();
   };
 
-  var sellingPoints = document.getElementsByClassName("selling-points")[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  var scrollDistance = $(".selling-points").offset().top - $(window).height
+  () + 200;
 
-  window.addEventListener("scroll", function(event) {
-    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
-      animatePoints(pointsArray);
+  $(window).scroll(function(event) {
+    if($(window).scrollTop() >= scrollDistance){
+      animatePoints();
     }
   });
-};
+});
